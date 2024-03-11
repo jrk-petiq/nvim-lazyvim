bb40abda2eb725e3b5011e88f6ff321f4fab5941
John Kowalsky <john.kowalsky@petiq.com>
2024-03-11 07:56:45 -0600

    add navigate in Insert mode

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,3 +5,13 @@ vim.keymap.set("n", "<leader>w", ":w!<CR>")
 vim.keymap.set("i", "jj", "<ESC>")
 vim.keymap.set("n", "<leader>q", ":q!<CR>")
 vim.keymap.set("n", "<leader>x", ":x!<CR>")
+
+-- go to beginning and end of line
+vim.keymap.set("i", "<C-b>", "<ESC>^i")
+vim.keymap.set("i", "<C-e>", "<End>")
+
+-- navigate within insert mode
+vim.keymap.set("i", "<C-h>", "<Left>")
+vim.keymap.set("i", "<C-l>", "<Right>")
+vim.keymap.set("i", "<C-j>", "<Down>")
+vim.keymap.set("i", "<C-k>", "<Up>")
